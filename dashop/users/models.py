70cf515119bf9d4aeea23e8e2e5da7b0cb2f8951
Robin.Wang <3Jg$94sbQ@q1>
2023-10-09 11:37:00 +0800

    feat(users): add address model

--- dashop/users/models.py
+++ dashop/users/models.py
@@ -17,3 +17,27 @@ class UserProfile(models.Model):
     class Meta:
         # 表名: 应用名_类名非驼峰
         db_table = "users_user_profile"
+
+
+class Address(models.Model):
+    """收货地址表"""
+    # 外键
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, verbose_name="用户")
+    # 收件人、地址、邮编、手机号
+    receiver = models.CharField(max_length=10, verbose_name="收件人")
+    address = models.CharField(max_length=100, verbose_name="地址")
+    postcode = models.CharField(max_length=6, verbose_name="邮编")
+    receiver_mobile = models.CharField(max_length=11, verbose_name="手机号")
+    # 标签[家、公司、宿舍]
+    tag = models.CharField(verbose_name="标签", max_length=10)
+    # 是否为默认地址
+    is_default = models.BooleanField(default=False, verbose_name="默认地址")
+    # 伪删除[0-未删除,1-删除]
+    is_delete = models.BooleanField(default=False, verbose_name="伪删除")
+    # 创建时间 更新时间
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    updated_time = models.DateTimeField(auto_now=True, verbose_name="更新时间")
+
+    class Meta:
+        # 修改表名: 应用名_类名非驼峰
+        db_table = "users_address"
