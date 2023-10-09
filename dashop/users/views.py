a13fd92a83cff6ef96a1040babe2d00aa5e5ba64
Robin.Wang <3Jg$94sbQ@q1>
2023-10-09 10:46:03 +0800

    feat(users): modify register func

--- dashop/users/views.py
+++ dashop/users/views.py
@@ -1,6 +1,11 @@
 import json
+import hashlib
+import time
 
+import jwt
 from django.http import JsonResponse, HttpRequest
+from users.models import UserProfile
+from dashop import settings
 
 
 def register(request: HttpRequest) -> JsonResponse:
@@ -31,4 +36,47 @@ def register(request: HttpRequest) -> JsonResponse:
     if len(phone) != 11:
         return JsonResponse({"code": 10102, "error": "手机号不合法"})
 
-    return JsonResponse({"code": 10100, "errmsg": ""})
+    # 3.确认用户名是否被占用
+    user_query = UserProfile.objects.filter(username=username)
+    print(f"user_query -> {user_query}")
+    # 被占用
+    if user_query:
+        return JsonResponse({"code": 10103, "error": "用户名已被占用"})
+
+    # 存入数据表
+    try:
+        user = UserProfile.objects.create(username=username, password=md5_string(password), email=email, phone=phone)
+        print(f"user -> {user}")
+    except Exception as e:
+        print(e)
+
+    # 签发token
+    token = make_token(username)
+
+    # 返回响应
+    result = {
+        "code": 200,
+        "username": username,
+        "token": token,
+        "carts_count": 0
+    }
+
+    return JsonResponse(result)
+
+
+def md5_string(s):
+    """
+    功能函数:md5加密
+    """
+    m = hashlib.md5()
+    m.update(s.encode())
+    return m.hexdigest()
+
+
+def make_token(username, expire=3600 * 24):
+    """
+    功能函数:签发token
+    """
+    payload = {"exp": time.time() + expire, "username": username}
+    key = settings.JWT_TOKEN_KEY
+    return jwt.encode(payload, key, algorithm="HS256")
