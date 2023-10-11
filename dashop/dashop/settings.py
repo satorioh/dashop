d4d9a896372b3253f82dd109cce89b6cd77487d6
Robin.Wang <3Jg$94sbQ@q1>
2023-10-11 14:29:51 +0800

    feat(project): add doppler

--- dashop/dashop/settings.py
+++ dashop/dashop/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,7 +81,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': 3306,
         'USER': 'root',
-        'PASSWORD': '1qaz2wsx',
+        'PASSWORD': DB_PASSWORD,
         'NAME': 'dashop',
     }
 }
